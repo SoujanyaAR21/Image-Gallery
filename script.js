a2fdb4259f07ea28ecf297f2798d3a2df32f6ea9
Soujanya A R <soujanyapoojary80@gmail.com>
2026-02-15 05:50:46 +0530

    Add openLightbox function to display images

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const galleryImages = document.querySelectorAll('.gallery img');
 let currentImageIndex = 0;
+
+function openLightbox(index) {
+      currentImageIndex = index;
+      document.getElementById('lightboxImage').src = galleryImages[index].src;
+      document.getElementById('lightbox').style.display = 'flex';
+    }
